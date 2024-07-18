87d58093a8372ec857fc8990d2fda2843426ef05
GiGiDKR <74984020+GiGiDKR@users.noreply.github.com>
2024-07-18 15:13:20 +0200

    Update to latest version

--- install_vscode.sh
+++ install_vscode.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-
 varname=$(basename $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/home/*)
 
-proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 apt update
-proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 apt install -y wget gpg software-properties-common apt-transport-https
-
-proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 rm -f packages.microsoft.gpg
+latest_version=$(curl -s https://api.github.com/repos/microsoft/vscode/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
 
 proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 apt update
-proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 apt install -y code
+
+proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 wget "https://update.code.visualstudio.com/${latest_version}/linux-deb-arm64/stable" -O code.deb
+
+proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 sudo -S apt install ./code.deb -y
+
+proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 rm code.deb
+
+proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 sudo -S apt install gpg software-properties-common apt-transport-https -y
+
+proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 sudo apt-key add -
 
 echo "[Desktop Entry]
 Version=1.0
 Type=Application
 Name=VS Code
 Comment=Code Editing. Redefined.
-Exec=proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 /usr/bin/code --no-sandbox
+Exec=proot-distro login debian --user $varname --shared-tmp -- env DISPLAY=:1.0 /usr/share/code/code --no-sandbox
 Icon=visual-studio-code
 Categories=Development;
 Path=
@@ -27,4 +29,4 @@ StartupNotify=false
 " > $HOME/Desktop/code.desktop
 
 chmod +x $HOME/Desktop/code.desktop
-cp $HOME/Desktop/code.desktop $HOME/../usr/share/applications/code.desktop
+cp $HOME/Desktop/code.desktop $HOME/../usr/share/applications/code.desktop 
